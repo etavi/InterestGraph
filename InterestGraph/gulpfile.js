46bdfcc88331bd52229bec6efb0fccc246bc5b2f
unknown <paunescu@PAUNESCU2.emea.cpqcorp.net>
2015-03-16 17:58:18 +0200

    message2

--- InterestGraph/gulpfile.js
+++ InterestGraph/gulpfile.js
@@ -1,5 +1,24 @@
+// Include Gulp
 var gulp = require('gulp');
-
-gulp.task('default', function() {
-  // place code for your default task here
+ // Include plugins
+var plugins = require("gulp-load-plugins")({
+	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+	replaceString: /\bgulp[\-.]/
 });
+ // Define default destination folder
+var dest = 'www/public/';
+
+/*
+gulp.src(plugins.mainBowerFiles())
+	.pipe(plugins.filter('*.js'))
+	.pipe(gulp.dest(dest + 'js'));
+*/
+
+gulp.task('js', function() {
+ 	var jsFiles = ['src/js/*'];
+ 	gulp.src(plugins.mainBowerFiles().concat(jsFiles))
+		.pipe(plugins.filter('*.js'))
+		.pipe(plugins.concat('main.js'))
+		.pipe(plugins.uglify())
+		.pipe(gulp.dest(dest + 'js'));
+ });
